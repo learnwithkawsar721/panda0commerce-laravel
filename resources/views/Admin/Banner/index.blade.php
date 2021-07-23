63a76e4bb31eced0ab0710579261b78afad79d0c
Md Kawsar <kawsar721g@gmail.com>
2021-07-23 16:52:24 +0600

    Banner Data Create

---
+++ resources/views/Admin/Banner/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Banner</li>
+                </ol>
+
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <a href="{{ route('banner.create') }}" class="btn btn-sm btn-success my-3">Add New</a>
+            <div class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Discrip</th>
+                            <th>Price</th>
+                            <th>Link</th>
+                            <th>Images</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Lorem ipsum dolor sit.</td>
+                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, repellat?</td>
+                            <td>$120</td>
+                            <td><a href="www.facebook.com">www.facebook.com</a></td>
+                            <td>img</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                    <button type="button" class="btn btn-danger">Left</button>
+                                    <button type="button" class="btn btn-warning">Middle</button>
+                                    <button type="button" class="btn btn-success">Right</button>
+                                </div>
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BannerController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('banner',BannerController::class);
+});
